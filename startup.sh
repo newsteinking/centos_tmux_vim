ef870a64c659ca238473ee567eb7bfce59a795bd
sean <newstein33@gmail.com>
2015-04-27 11:22:11 +0900

    inital

--- startup.sh
+++ startup.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
 echo "copy all ~/       ===================================>>>"
-cp -R ./*  ~/
-cp -R ./.*  ~/
+cp _alias ~/.alias
+cp _minttyrc ~/.minttyrc
+cp _tmux.conf ~/.tmux.conf
+cp _vimrc ~/.vimrc
+cp _zshrc ~/.zshrc
+cp -R ./project ~/
+rm -rf ~/startup.sh
+
 echo "copy all complete ===================================>>>"
 
 echo "yum  install tmux  ===================================>>>"
 yum install -y  tmux
 
+
+echo "clone solarized   ===================================>>>"
+git clone https://github.com/altercation/solarized.git ~/.vim/bundle/solarized
+
 echo "clone vim vundle   ===================================>>>"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "copy solarized.vim to colors dir   ===================================>>>"
+cp ~/.vim/bundle/solarized/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
 
 echo "install vim plugin     ===================================>>>"
 vim +PluginInstall +qall
 
-echo "clone solarized   ===================================>>>"
-git clone https://github.com/altercation/solarized.git ~/.vim/bundle/solarized
+
 
 echo "Good Coding Life for You !!!!"
 
